f1b4c847c62a135f4680ccfcb1fa78bb4715128b
Evens <zhangzhifan.beat@gmail.com>
2015-11-20 23:24:29 +0800

    add some test code

--- utils/sigslot/main.cpp
+++ utils/sigslot/main.cpp
@@ -3,12 +3,27 @@
 #include <string>
 #include "sigslot.h"
 
+
+using namespace std;
+
 class Sender  {
 public:
   sigslot::signal2<std::string, std::time_t> SignalDanger;
 
+  sigslot::signal0<> SignalCallv;
+
+  sigslot::signal1<string> SignalCalls;
+
   void Panic(){
     SignalDanger("Help!", std::time(0));
+  }
+
+  void TCallv() {
+    SignalCallv();
+  }
+
+  void TCalls() {
+    SignalCalls("helloworld");
   }
 };
 
@@ -16,6 +31,8 @@ class Receiver : public sigslot::has_slots<> {
 public:
   Receiver(Sender& sender){
     sender.SignalDanger.connect(this, &Receiver::OnDanger);
+    sender.SignalCallv.connect(this, &Receiver::OnCall);
+    sender.SignalCalls.connect(this, &Receiver::onCall2);
   }
 
   void OnDanger(std::string message, std::time_t time){
@@ -24,6 +41,16 @@ public:
       std::cout << "Call the police" << std::endl;
     }
   }
+
+  void OnCall() {
+
+    cout << "on call !"<< endl;
+  }
+
+  void onCall2(string s) {
+    cout << "on call 2!" << s.c_str() <<endl;
+  }
+
 };
 
 int main(int argc, char** argv)
@@ -31,5 +58,9 @@ int main(int argc, char** argv)
   Sender sender;
   Receiver receiver(sender);
   sender.Panic();
+  sender.TCallv();
+  sender.TCalls();
   return 0;
 }
+
+
