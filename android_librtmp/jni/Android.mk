7d1c4b0fdd884d5b9c0c46b90dbef07da84cdf2a
zzf <zzf@sina.cn>
2015-12-03 11:29:56 +0800

    android librtmp

---
+++ android_librtmp/jni/Android.mk
@@ -0,0 +1,42 @@
+LOCAL_PATH:= $(call my-dir)
+
+SSL := ${LOCAL_PATH}/../openssl/$(TARGET_ARCH_ABI)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libssl
+LOCAL_SRC_FILES := ${SSL}/libssl.a
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcrypto
+LOCAL_SRC_FILES := ${SSL}/libcrypto.a
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+
+
+LOCAL_SRC_FILES:= \
+	${LOCAL_PATH}/../librtmp/amf.c \
+	${LOCAL_PATH}/../librtmp/hashswf.c \
+	${LOCAL_PATH}/../librtmp/log.c \
+	${LOCAL_PATH}/../librtmp/parseurl.c \
+	${LOCAL_PATH}/../librtmp/rtmp.c
+
+
+
+LOCAL_CFLAGS += -I$(SSL)/include
+#LOCAL_LDLIBS += -L$(SSL)/libs/$(TARGET_ARCH_ABI)
+LOCAL_LDLIBS += -ldl -lz
+
+LOCAL_STATIC_LIBRARIES := \
+	libssl \
+	libcrypto 
+
+
+
+LOCAL_MODULE := librtmp
+
+include $(BUILD_SHARED_LIBRARY)
+#include $(BUILD_STATIC_LIBRARY)
+
+
---
+++ android_librtmp/jni/Application.mk
@@ -0,0 +1,4 @@
+
+APP_PLATFORM := android-8
+
+
