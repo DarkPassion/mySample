195e016213d8db76194ec978a58750a85eeed55e
zhangzhifan <zhangzhifan@myhug.cn>
2017-03-23 11:00:45 +0800

    format asytle --style=bsd

--- socket_server/http_client.c
+++ socket_server/http_client.c
@@ -10,35 +10,38 @@
 const int httpc_id = 300;
 
 static void *
-_poll(void * ud) {
-	struct socket_server *ss = ud;
-	struct socket_message result;
-	for (;;) {
-		int type = socket_server_poll(ss, &result, NULL);
-		// DO NOT use any ctrl command (socket_server_close , etc. ) in this thread.
-		switch (type) {
-		case SOCKET_EXIT:
-			return NULL;
-		case SOCKET_DATA:
-			printf("message(%lu) [id=%d] size=%d\n",result.opaque,result.id, result.ud);
+_poll(void * ud)
+{
+    struct socket_server *ss = ud;
+    struct socket_message result;
+    for (;;)
+    {
+        int type = socket_server_poll(ss, &result, NULL);
+        // DO NOT use any ctrl command (socket_server_close , etc. ) in this thread.
+        switch (type)
+        {
+        case SOCKET_EXIT:
+            return NULL;
+        case SOCKET_DATA:
+            printf("message(%lu) [id=%d] size=%d\n",result.opaque,result.id, result.ud);
             printf("http client recv: %s \n", result.data);
-			free(result.data);
-			break;
-		case SOCKET_CLOSE:
-			printf("close(%lu) [id=%d]\n",result.opaque,result.id);
-			break;
-		case SOCKET_OPEN:
-			printf("open(%lu) [id=%d] %s\n",result.opaque,result.id,result.data);
-			break;
-		case SOCKET_ERROR:
-			printf("error(%lu) [id=%d]\n",result.opaque,result.id);
-			break;
-		case SOCKET_ACCEPT:
-			printf("accept(%lu) [id=%d %s] from [%d]\n",result.opaque, result.ud, result.data, result.id);
+            free(result.data);
+            break;
+        case SOCKET_CLOSE:
+            printf("close(%lu) [id=%d]\n",result.opaque,result.id);
+            break;
+        case SOCKET_OPEN:
+            printf("open(%lu) [id=%d] %s\n",result.opaque,result.id,result.data);
+            break;
+        case SOCKET_ERROR:
+            printf("error(%lu) [id=%d]\n",result.opaque,result.id);
+            break;
+        case SOCKET_ACCEPT:
+            printf("accept(%lu) [id=%d %s] from [%d]\n",result.opaque, result.ud, result.data, result.id);
             socket_server_start(ss, 1000, result.ud);
-			break;
-		}
-	}
+            break;
+        }
+    }
 }
 
 static void
@@ -54,7 +57,8 @@ test_http(struct socket_server* ss, const char* addr, int port)
     char buf[] = "GET / HTTP/1.1\r\nHost: www.baidu.com\r\nCache-Control: max-age=0\r\nConnection: keep-alive\r\nAccept: text/html\r\nUser-Agent: Mozilla/5.0\r\n\r\n";
     char* sbuf = strdup(buf);
     int64_t ret = socket_server_send(ss, httpc, sbuf, sizeof(buf));
-    if (ret < 0) {
+    if (ret < 0)
+    {
         printf("test httpc socket server send fail! \n");
     }
 
@@ -65,13 +69,14 @@ test_http(struct socket_server* ss, const char* addr, int port)
 
 
 int
-main() {
-	struct sigaction sa;
-	sa.sa_handler = SIG_IGN;
-	sigaction(SIGPIPE, &sa, 0);
+main()
+{
+    struct sigaction sa;
+    sa.sa_handler = SIG_IGN;
+    sigaction(SIGPIPE, &sa, 0);
 
-	struct socket_server * ss = socket_server_create();
+    struct socket_server * ss = socket_server_create();
     test_http(ss, "127.0.0.1", 8080);
-	socket_server_release(ss);
-	return 0;
+    socket_server_release(ss);
+    return 0;
 }
